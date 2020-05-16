c5e9609ab2435d9e07b74bc6d04d4e871f518499
Silva Neto <silva.neto.sistemas@gmail.com>
2020-05-15 22:07:55 -0300

    Ajustando estilo e adicionando blocos

--- src/app/modules/public/home/home.component.ts
+++ src/app/modules/public/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    $(window).on("load scroll", function(){
+      $(".mostrar").each(function(){
+        var el = $(this);
+        var eleHeight = el.outerHeight(); // altura da div
+        var eleTopo = el.offset().top; // distancia da div ao topo do documento
+        var scrlTopo = $(window).scrollTop(); // quanto foi rolada a janela
+        var distance = eleTopo-scrlTopo; // distancia da div ao topo da janela
+        var altJanela = window.innerHeight; // altura da janela
+        if(distance <= altJanela-(eleHeight/2)) {
+          el.animate({ 'opacity': 1 }, 600);
+        }
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
