8ccaa9ff9dc86f6e3bcc475f5bf6c297dd987184
Silva Neto <silva.neto.sistemas@gmail.com>
2020-05-20 18:07:27 -0300

    Finalizando funcionalidades e estilização do portfólio

--- src/app/modules/public/home/home.component.ts
+++ src/app/modules/public/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
@@ -8,20 +8,32 @@ import * as $ from 'jquery';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('jobs') jobs: ElementRef;
+  @ViewChild('group') group: ElementRef;
+  @ViewChild('services') services: ElementRef;
+  @ViewChild('about') about: ElementRef;
+  @ViewChild('contact') contact: ElementRef;
+
   constructor() {
-    $(window).on("load scroll", function(){
-      $(".mostrar").each(function(){
+    $(window).on("load scroll", function () {
+      $(".mostrar").each(function () {
         var el = $(this);
         var eleHeight = el.outerHeight(); // altura da div
         var eleTopo = el.offset().top; // distancia da div ao topo do documento
         var scrlTopo = $(window).scrollTop(); // quanto foi rolada a janela
-        var distance = eleTopo-scrlTopo; // distancia da div ao topo da janela
+        var distance = eleTopo - scrlTopo; // distancia da div ao topo da janela
         var altJanela = window.innerHeight; // altura da janela
-        if(distance <= altJanela-(eleHeight/2)) {
-          el.animate({ 'opacity': 1 }, 600);
+        if (distance <= altJanela - (eleHeight / 2)) {
+          el.animate({ 'opacity': 1 }, 750);
         }
       });
     });
+  }
+
+  navigateTo(control: string) {
+    if (control) {
+      this[control].nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   ngOnInit(): void {
